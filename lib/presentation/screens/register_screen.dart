1f32b26e23922478222e9e551c6c1058d26f34bd
Juan Carlos Medina <carlos.medina@uadec.edu.mx>
2023-07-03 13:17:00 -0600

    Restricciones para login finalizadas

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -19,32 +20,101 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: SingleChildScrollView(
-          child: Column(
+    return const SafeArea(
+      child:  Padding(
+        padding:  EdgeInsets.symmetric(horizontal: 10),
+        child:  SingleChildScrollView(
+          child:  Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const FlutterLogo(size: 100),
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
-              
-              const SizedBox(height: 40),
-
-              FilledButton.tonalIcon(
-                onPressed: () {}, 
-                icon: const Icon(Icons.save),
-                label: const Text('Crear Usuario'),
-                ),
-                
-
-              const SizedBox(height: 20)
+            children:  [
+              FlutterLogo(size: 100),
+              _RegisterForm(),
+              SizedBox(height: 40),
+              SizedBox(height: 20)
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _RegisterForm extends StatefulWidget {
+  const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  
+  // Me permite enlazar el key con el form lo cual me permite tener el control de todo el form con ese key.
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? username = '';
+  String? email = '';
+  String? password = '';
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+     key: _formKey,
+        child: Column(
+      children: [
+        
+        const SizedBox(height: 20),
+
+        CustomTextField(
+          label: 'Nombre de usuario',
+          onChanged: (value) => username = value,
+          validator: (value) {
+            if(value == null || value.isEmpty) return 'Campo requerido';
+            if(value.trim().isEmpty) return 'Campo requerido';
+            if(value.length < 6 ) return 'El usuario debe tener más de 6 letras';
+            return null;
+          },
+        ), 
+        const SizedBox(height: 20),
+        CustomTextField(
+          label: 'Correo electronico',
+          onChanged: (value) => email = value,
+          validator: (value) {
+            if(value == null || value.isEmpty) return 'Campo requerido';
+            if(value.trim().isEmpty) return 'Campo requerido';
+           final emailRegExp = RegExp(
+              r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+            );
+            if(!emailRegExp.hasMatch(value)) return 'Formato de correo inválido.';
+            return null;
+          },
+        ),
+
+        const SizedBox(height: 20),
+        CustomTextField(
+          label: 'Contraseñas',
+          obscureText: true,
+          onChanged: (value) => password = value,
+          validator: (value) {
+            if(value == null || value.isEmpty) return 'Campo requerido';
+            if(value.trim().isEmpty) return 'Campo requerido';
+            if(value.length < 6 ) return 'El usuario debe tener más de 6 letras';
+            return null;
+          },
+        ),
+
+        const SizedBox(height: 20),
+        FilledButton.tonalIcon(
+          onPressed: () {
+            final isValid = _formKey.currentState!.validate();
+            if( !isValid ) return;
+
+          },
+          icon: const Icon(Icons.save),
+          label: const Text('Crear Usuario'),
+        ),
+      ],
+    ));
   }
 }
